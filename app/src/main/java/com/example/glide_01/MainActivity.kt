9d4ab2136ebc73b3f3e852520f8ce8944b939d25
13570524658 <GitHub9931029>
2020-08-26 04:16:59 +0800

    占位图

--- app/src/main/java/com/example/glide_01/MainActivity.kt
+++ app/src/main/java/com/example/glide_01/MainActivity.kt
@@ -1,17 +1,7 @@
 package com.example.glide_01
 
-import android.R.attr.radius
-import android.graphics.drawable.Drawable
 import android.os.Bundle
-import androidx.annotation.Nullable
 import androidx.appcompat.app.AppCompatActivity
-import com.bumptech.glide.Glide
-import com.bumptech.glide.load.MultiTransformation
-import com.bumptech.glide.load.resource.bitmap.CircleCrop
-import com.bumptech.glide.load.resource.bitmap.RoundedCorners
-import com.bumptech.glide.request.RequestOptions
-import com.bumptech.glide.request.target.SimpleTarget
-import com.bumptech.glide.request.transition.Transition
 import kotlinx.android.synthetic.main.activity_main.*
 
 
